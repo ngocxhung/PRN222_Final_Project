745137fe218d929fb833948ecaa2dc0838d1727d
Nguyen Tuan Vinh Ninh K17 HL <ninhntvhe172162@fpt.edu.vn>
2025-03-19 17:32:10 +0700

    Controller: Add ReportAndStatisticController.cs; View: Add ReportAndStatistic

--- PRN222_Project/PRN222_Project/obj/Debug/net8.0/PRN222_Project.AssemblyInfo.cs
+++ PRN222_Project/PRN222_Project/obj/Debug/net8.0/PRN222_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN222_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b831296e317c1518401468db45a82e7ba2e74042")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN222_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN222_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
